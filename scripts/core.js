175aa2ca0825ff7ef91e0c956747bb474c997175
hig19011 <hig19011@byui.edu>
2020-01-10 00:12:34 -0700

    cleaned up css, html and images

--- scripts/core.js
+++ scripts/core.js
@@ -1,2 +1,5 @@
 var lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = lastModified;
+var modified = new Date(lastModified);
+var currentYear = modified.getFullYear();
+document.getElementById("copywriteYear").innerHTML = currentYear;
