4b4ea89931721e3d345e8f0b0d527ecbd29b6cd2
hig19011 <hig19011@byui.edu>
2020-01-25 01:53:09 -0700

    Initial work for Lesson 3

---
+++ lesson3/js/core.js
@@ -0,0 +1,6 @@
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastModified;
+let modified = new Date(lastModified);
+let currentYear = modified.getFullYear();
+document.getElementById("copywriteYear").innerHTML = currentYear;
+
