f91c7df2ef173b44efe9afaacd65d390bf8616c1
hig19011 <hig19011@byui.edu>
2020-02-08 17:55:46 -0700

    change styles for better responsiveness

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -2,6 +2,14 @@ let now = new Date();
 let longDate = now.toLocaleDateString('en-us', {weekday: 'long'}) + ", " + now.toLocaleDateString('en-us', { day:'numeric'}) + " " + now.toLocaleDateString('en-us', {month:'long'}) + " " + now.toLocaleDateString('en-us', {year:'numeric'});
 document.getElementById("currentDate").innerHTML = longDate;
 
+if(now.getDay() == 6)
+{
+  var announcements = document.getElementsByClassName("announcement");
+  var announcement = announcements[0];
+  if(announcement != undefined)
+    announcement.classList.remove("hide");
+}
+
 function toggleMenu() {
   console.log(document.getElementById("primaryNav").classList);
   document.getElementById("primaryNav").classList.toggle("hide");
