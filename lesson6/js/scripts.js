3c72319be70017cbe96334d91b41b2cddf459447
hig19011 <hig19011@byui.edu>
2020-02-15 12:01:20 -0700

    Updated windspeed calculation and display

--- lesson6/js/scripts.js
+++ lesson6/js/scripts.js
@@ -11,18 +11,8 @@ if(now.getDay() == 5)  //Show if Friday
     announcement.classList.remove("hide");
 }
 
-// find and display the windchill 
-let temp = document.getElementById("temp").textContent;
-let windSpeed = document.getElementById("windSpeed").textContent;
-let windChill = calculateWindChillFactor(temp,windSpeed);
-document.getElementById("windChill").textContent = windChill.toFixed(0);
-
-
 function toggleMenu() {
   console.log(document.getElementById("primaryNav").classList);
   document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-function calculateWindChillFactor(t,s){
-  return 35.74 + .6215*t - 35.75*Math.pow(s,0.16) + .4275*t*Math.pow(s,.016)
-}
