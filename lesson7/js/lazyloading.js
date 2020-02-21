086dd281294c7cab27f123b7fa63c3ba56a9ad69
hig19011 <hig19011@byui.edu>
2020-02-20 18:05:54 -0700

    Update Title add shadow to header

--- lesson7/js/lazyloading.js
+++ lesson7/js/lazyloading.js
@@ -1,25 +1,43 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
+//Get all images on page that will be lazy loaded
+let imagesList = document.querySelectorAll('img[data-src]');
+
+//for the given image set the src to the data-src value, delete data-src
+const loadImages = (img) => {
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = () => {
+    img.removeAttribute('data-src');
   };
 };
 
+// require at least 1/3 of the image be on the screen before loading
+// used to help demo the feature
+const imageOptions = {
+  threshold: .333
+};
+
+//If we can use the IntersectionObserver
 if('IntersectionObserver' in window) {
-  const observer = new IntersectionObserver((items, observer) => {
+  // setup the action to occur when the observer fires a change
+  const imageObserver = new IntersectionObserver((items, observer) => {
+    // go through all observed items
     items.forEach((item) => {
+      // if the item is showing on the screen (modified by imageOptions)
       if(item.isIntersecting) {
+        //load image
         loadImages(item.target);
+        //stop checking for changes on this image because it has already loaded.
         observer.unobserve(item.target);
       }
     });
+  }, imageOptions);
+  // register each image for observation
+  imagesList.forEach((img) => {
+    imageObserver.observe(img);
   });
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
+} 
+// otherwise load all images now.
+else {
+  imagesList.forEach((img) => {
     loadImages(img);
   });
 }
