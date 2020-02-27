499142a5de163a51c688464080bfe393483af0ca
hig19011 <hig19011@byui.edu>
2020-02-27 00:40:59 -0700

    add lesson 8

---
+++ lesson8/js/scripts.js
@@ -0,0 +1,18 @@
+// display banner for Firday add (or not)
+let now = new Date();
+let longDate = now.toLocaleDateString('en-us', {weekday: 'long'}) + ", " + now.toLocaleDateString('en-us', { day:'numeric'}) + " " + now.toLocaleDateString('en-us', {month:'long'}) + " " + now.toLocaleDateString('en-us', {year:'numeric'});
+document.getElementById("currentDate").innerHTML = longDate;
+
+if(now.getDay() == 5)  //Show if Friday
+{
+  var announcements = document.getElementsByClassName("announcement");
+  var announcement = announcements[0];
+  if(announcement != undefined)
+    announcement.classList.remove("hide");
+}
+
+function toggleMenu() {
+  console.log(document.getElementById("primaryNav").classList);
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+
